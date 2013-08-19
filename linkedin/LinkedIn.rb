639e4ae32e7149fe4bd05cb18d0da4fc68865b8b
Iain Walker <immachumm@gmail.com>
2013-08-19 21:17:03 +1200

    LinkedIn Scraper now working, scraping core values. Was quite quick to implement..

--- linkedin/LinkedIn.rb
+++ linkedin/LinkedIn.rb
@@ -2,6 +2,8 @@ require "rest-client"
 require "nokogiri"
 require "uri"
 require "logger"
+require 'csv'
+require 'work_queue'
 require './UserAgent'
 require './Profile'
 
@@ -16,23 +18,34 @@ class LinkedInSearch
 		@user_agent = UserAgent.new
 	end
 
+	#loop through the given names file, and scrape the results that we find on twitter
 	def search_through_names
+		profile_list = Array.new
+		i=0.0
 		nf = File.open(@name_file)
 		nf.each_line do |line|
 			if line != nil
+				i = i + 1.0
 				url_to_search = construct_search_url(line.chomp)
+				if url_to_search != false
 				url_list_to_scrape = get_profile_url_list(url_to_search)
-				url_list_to_scrape.each do |profile_url|#could parrelize at this level.
-					scrape_profile(profile_url.to_s)
-				end	
+				if url_list_to_scrape != nil #just check that there are profiles to scrape in this list...
+					url_list_to_scrape.each do |profile_url|#could parrelize at this level.
+						i=i+0.1
+						profile_list << scrape_profile(profile_url.to_s,i)
+					end	
+				end
+			end
 			end
 		end
-	end
+		save_profiles_to_xml(profile_list)
+	end	
 
 	#call the profile-level scraper code to actually scrape and store this profile.
-	def scrape_profile(profile_url)
-		profile = Profile.new(profile_url,@user_agent)
+	def scrape_profile(profile_url,id)
+		profile = Profile.new(profile_url,@user_agent,id)
 		profile.scrape
+		return profile
 	end
 
 	#simple helper method to construct the search URL
@@ -41,11 +54,13 @@ class LinkedInSearch
 		base_url = "http://www.linkedin.com/pub/dir/?"
 		if first != nil
 			base_url << "first=#{first}"
+		else
+			return false#require a first name.
 		end
 		if last != nil 
 			base_url<<"&last=#{last}"
 		else
-			base_url<< "&last=Search"
+			base_url<< "&last="
 		end
 		base_url<< "&search=Search&searchType=fps"
 		puts "url... #{base_url}"
@@ -55,6 +70,7 @@ class LinkedInSearch
 	#loads the list of names, returns a list of URLs that
 	#can be used to view profiles.
 	def get_profile_url_list(url)
+		begin
 		url_list = Array.new
 		#first, load the page..
 		resp = RestClient.get(url,:user_agent => @user_agent.get_user_agent.to_s)
@@ -65,7 +81,51 @@ class LinkedInSearch
 			url_list << href
 		end
 		return url_list
+		rescue Exception => e#it is possible that we will get 404 Not Found Exceptions at this point,
+			#given any names that do not exist on LinkedIn. Unlikely but possible.
+			puts "Exception #{e}"
+		end
 	end
+
+	#save the given profiles to a single file
+	#xml format.
+	def save_profiles_to_xml(profile_list)
+		puts 'saving results to xml document'
+		builder = Nokogiri::XML::Builder.new do |xml|
+			xml.profiles{
+				profile_list.each do |p|			
+				xml.profile(:id=>p.get_id.to_s){		
+					xml.num_connections_ p.get_num_connections
+					xml.num_recommendations_ p.get_num_recommendations
+					xml.current_position_ p.get_current_position
+					xml.skills(:num_skills => p.get_skills.length){
+						p.get_skills.each do |skill|
+							xml.skill_ skill
+						end
+					}
+					xml.groups(:num_groups => p.get_groups.length){
+						p.get_groups.each do |group|
+							xml.group_ group
+						end
+					}
+					}						
+				end
+
+			}
+		end
+		time = Time.now
+		t_f = time.mon.to_s+time.day.to_s+time.hour.to_s+time.min.to_s
+		#Dir.mkdir("#{@results_dir}/run#{time}")
+		file = File.open("#{@results_dir}/run#{t_f}.xml","w")
+		file.write(builder.to_xml.to_s)
+	end
+
+	#save the given profiles to a single file.
+	#csv format.
+	def save_profiles_to_csv(profile_list)
+		#TODO
+	end
+
 
 end
 
--- linkedin/Profile.rb
+++ linkedin/Profile.rb
@@ -5,9 +5,10 @@ require 'nokogiri'
 
 class Profile
 
-	def initialize(url,user_agent)
+	def initialize(url,user_agent,id)
 		@url = url
 		@user_agent = user_agent
+		@id = id
 	end
 
 	def scrape
@@ -15,36 +16,81 @@ class Profile
 		resp = RestClient.get(@url,:user_agent => @user_agent.get_user_agent.to_s)
 		profile_doc = Nokogiri::HTML(resp)
 		#now that we have the document, can convert this into meaningful data...
-		@num_recommendations = get_recommendations(profile_doc)
-		@num_connections = get_num_connections(profile_doc)
-		@current_position = get_current_position(profile_doc)
-		@skill_list = get_skill_list(profile_doc)
+		@num_recommendations = find_recommendations(profile_doc)
+		@num_connections = find_num_connections(profile_doc)
+		@current_position = find_current_position(profile_doc)
+		@skill_list = find_skill_list(profile_doc)
+		@groups_associations = find_groups_associations(profile_doc)
+	end
+
+	def find_name(doc)
+
 	end
 
 	#the number of people that have recommended this person
-	def get_recommendations(doc)
+	def find_recommendations(doc)
 		num_recommendations = doc.xpath('//*[@id="overview"]/dd[2]/p/strong/text()')
-		puts "Number of recommendations #{num_recommendations}"
-		return num_recommendations
+		#puts "Number of recommendations #{num_recommendations}"
+		return num_recommendations.to_s.gsub(/\s+/, ' ')
 	end
 
-	def get_num_connections(doc)
+	def find_num_connections(doc)
 		num_connections = doc.xpath('//*[@id="overview"]/dd[3]/p/strong/text()')
-		puts "number of connections #{num_connections}"
-		return num_connections
+		#puts "number of connections #{num_connections}"
+		return num_connections.to_s.gsub(/\s+/, ' ')
 	end
 
-	def get_current_position(doc)
+	def find_current_position(doc)
 		current_position = doc.xpath('//*[@id="overview"]/dd[1]/ul/li/text()').to_s.chomp.strip
-		puts "current position #{current_position}"
-		return current_position
+		#puts "current position #{current_position}"
+		return current_position.to_s.gsub(/\s+/, ' ')
 	end
 
 	#get the skills of the person
-	def get_skill_list(doc)
+	def find_skill_list(doc)
+		skill_list = Array.new
 		doc.xpath('//*[@id="skills-list"]/li').each do |li|
 			skill = li.xpath('./span/text()').to_s.strip
-			puts "skill #{skill}"
+			fixed_skill = skill.gsub(/\s+/, ' ')
+			#puts "skill #{skill}"
+			skill_list << fixed_skill
 		end
+		return skill_list
+	end
+
+	def find_groups_associations(doc)
+		groups = Array.new
+		doc.xpath('//*[@id="pubgroups"]/ul/li').each do |li|
+			org = li.xpath('./div/a/strong/text()').to_s.strip
+			groups << org.gsub(/\s+/, ' ')
+		end
+		return groups
+	end
+
+	############################
+	#GETTERS/SETTERS
+	############################
+	def get_num_recommendations
+		return @num_recommendations
+	end
+
+	def get_num_connections
+		return @num_connections
+	end
+
+	def get_current_position
+		return @current_position
+	end
+
+	def get_skills
+		return @skill_list
+	end
+
+	def get_groups
+		return @groups_associations
+	end
+
+	def get_id
+		return @id
 	end
 end
