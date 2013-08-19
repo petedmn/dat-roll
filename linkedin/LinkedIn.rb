c8fa7cf0aa82fbef774674ac189ecbd3f144fdf2
Iain Walker <immachumm@gmail.com>
2013-08-19 19:35:25 +1200

    additions to the linkedin scraper

--- linkedin/LinkedIn.rb
+++ linkedin/LinkedIn.rb
@@ -1,47 +1,82 @@
-require "open-uri"
 require "rest-client"
-require "crack"
-require "hpricot"
 require "nokogiri"
 require "uri"
-require 'openssl'
 require "logger"
+require './UserAgent'
 
-class LinkedInScraper
-		def initialize(url)
-		@url = URI.escape(url.to_s)	
-		#switch user agents for every single new scraper.
-		@userAgent = UserAgent.new
+#provides functionality to search for profiles, based on first/last names,
+#using the linked in search mechanism
+class LinkedInSearch
+
+	def initialize(name_file,results_directory)
+		RestClient.proxy = ENV['http_proxy']
+		@name_file = name_file
+		@results_dir = results_directory	
+		@user_agent = UserAgent.new
 	end
 
-	def scrape
-		puts "scraping..." + @url.to_s
-		@document = fetch_page
-		linkedInItem = LinkedInItem.new(@document,@resp)
-		return linkedInItem
+	def search_through_names
+		nf = File.open(name_file)
+		nf.each_line do |line|
+			if line != nil
+				url_to_search = construct_search_url(line.chomp)
+				url_list_to_scrape = get_profile_url_list#could parrelize at this level.
+				url_list_to_scrape.each do |profile_url|
+					scrape_profile(profile_url)
+				end	
+			end
+		end
 	end
 
-	def fetch_page
-		@resp = RestClient.get(@url,:user_agent => @userAgent.get_user_agent.to_s)
-		@document = Nokogiri::HTML(@resp)
-		return @document
+	#call the profile-level scraper code to actually scrape and store this profile.
+	def scrape_profile(profile_url)
+		profile = Profile.new(profile_url,@user_agent)
+		profile.scrape
 	end
-end
 
+	#simple helper method to construct the search URL
+	def construct_search_url(first,last=nil)		
+		#construct the search URL
+		base_url = "http://www.linkedin.com/pub/dir/?"
+		if first != nil
+			base_url << "first=#{first}"
+		end
+		if last != nil 
+			base_url << "&last=#{last}"
+		else
+			base_url << "&last=Search"
+		end
+		base_url << "&search=Search&searchType=fps"
+		puts "url... #{base_url}"
+		return base_url
+	end
 
-class UserAgent
+	#loads the list of names, returns a list of URLs that
+	#can be used to view profiles.
+	def get_profile_url_list
+		#first, load the page..
+		resp = RestClient.get(@base_url,:user_agent => @user_agent.get_user_agent.to_s)
+		doc = Nokogiri::HTML(@resp)
+		xpath = '//*[@id="result-set"]/ol'
+		doc.xpath(xpath).each do |li|
+			puts li
+		end
+	end
 
 end
 
 class CommandLineInterface
-	def initialize
+	def initialize(name_file,results_dir)
 		$logger = Logger.new('logger.log')
-		@name_list = LinkedInSearch.new("facebook_urls.txt")
-	end
-
-	def set_proxy(proxyname,user,password)
-		RestClient.proxy = proxyname
-	end
+		@name_list = LinkedInSearch.new(name_file,results_dir)
+	end	
 end
 
-cli = CommandLineInterface.new
+if ARGV[0] == nil
+	puts "USAGE - ruby LinkedIn.rb [Filename]"
+	exit 1
+end
+name_file = ARGV[0]
+results_dir = '/vol/projects/kris/OpenRep/Iain/linkedin/results'
+
+cli = CommandLineInterface.new(name_file,results_dir)
--- linkedin/Profile.rb
+++ linkedin/Profile.rb
@@ -1,33 +1,22 @@
 require 'rest-client'
 require 'nokogiri'
 require 'work_queue'
+require 'nokogiri'
 
 class Profile
 
-	def initialize
-		
+	def initialize(url,user_agent)
+		@url = url
+		@user_agent = user_agent
 	end
 
 	def scrape
-
+		#first, get the document... No need for infinite scroll on LinkedIn which is nice.
+		resp = RestClient.get(@url,:user_agent => @user_agent.get_user_agent.to_s)
+		profile_doc = Nokogiri::HTML(resp)
+		#now that we have the document, can convert this into meaningful data...
 	end
 
-end
 
-class LinkedInItem
-	def initialize(content,raw)
-		@raw = raw
-		@content = content		
-	end
 
-	def write_to_file(file_name,directory_name)
-		begin
-			Dir::mkdir(directory_name)
-		rescue Exception=>e
-		end
-
-		file = File.open(directory_name+"/"+file_name,"w")
-		file.write(@raw.to_s)
-		file.close
-	end
 end
