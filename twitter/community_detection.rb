f0b543247729925253a06188d42c4b1a49d43a42
Iain Walker <immachumm@gmail.com>
2013-09-06 18:36:28 +1200

    added a community detection module. This requires authentication details, and will require additional classes in order to loop through peoples follower/following lists, and go through retweet lists on twitter.

---
+++ twitter/community_detection.rb
@@ -0,0 +1,32 @@
+require 'nokogiri'
+require ''
+
+
+class Crawler
+
+	def initialize()
+
+	end
+
+	def 
+
+	#we need to loop through profiles, and gather follower/following details for these profiles
+
+	#then, we can go through tweets and look at who is retweeting these. 
+
+end
+
+
+#class to handle authentication details on twitter.
+class Authenticate
+
+	def initialize()
+
+	end
+
+end
+
+#handles the interface with the user
+class CommandLineInterface
+
+end
