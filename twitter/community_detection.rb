2438c4f6df59c517ba6c0ed57d50916ddff9c97a
Iain Walker <immachumm@gmail.com>
2013-09-10 22:33:44 +1200

    followers loading, infinite scroll should be working soon.

--- twitter/community_detection.rb
+++ twitter/community_detection.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'mechanize'
 require 'rest-client'
+require 'json'
 require_relative './UserAgent'
 
 class Crawler
@@ -21,8 +22,11 @@ class Crawler
 		#first, load the base profile.
 		page = search_page(@agent,@profile_name)
 		
-		#now click the first link on the page, in order to access this page.
+		#load base_followers
+		base_followers(@agent,@profile_name)
 
+		#load base_following
+		base_following(@agent,@profile_name)
 	end
 
 	#search for a page with the given profile name
@@ -37,12 +41,75 @@ class Crawler
 		return profile_page
 	end
 
-	def base_followers
+	#return a list of the profiles of everyone who follows this person.
+	def base_followers(agent,profile_name)
+		follower_names = Array.new		
+		followers_page = agent.click(agent.page.link_with(:href => "/#{profile_name}/followers"))
+		
+		#Now use RestClient in order to do some manual manipulation of cookies.
 
+		#fetch the authentication token from agent's cookies.
+		#It is somewhat of a struggle to get the first cookies into good representation, but after that it is a breeze.
+		auth_token = nil		
+		agent.cookies.each do |val|
+			if(val.to_s.start_with? 'auth_token') then
+				auth_token = val.to_s.gsub("auth_token=", "")
+			end
+		end			
+
+		response = RestClient.get(followers_page.uri.to_s,
+									{:cookies => {:auth_token => auth_token.to_s,:secure_session => "true"}})
+		#now loop over some rest-client requests, at each step parsing the response.
+
+		#so parse the profiles which are available at the base level.
+		doc = Nokogiri::HTML(response)
+		profiles_xpath = '//*[@id="stream-items-id"]/li'
+		doc.xpath(profiles_xpath).each do |node|
+			name_xpath = './div/div[2]/div/a/@href'
+			#remove the '/' character
+			name = node.xpath(name_xpath).to_s.gsub("/","")
+			follower_names << name			
+		end		
+
+		#ok we have all of the profiles available at this base level. Now, need to deal with inifinite scroll.		
+		#the data cursor is a value that is necessary when dealing with 
+		data_cursor_xpath = '//*[@id="timeline"]/div[2]/@data-cursor'
+		data_cursor = doc.xpath(data_cursor_xpath).to_s
+		puts "data cursor #{data_cursor}"
+		
+		has_more = true
+		until has_more == false do
+			has_more = parse_extra_followers(data_cursor,profile_name,follower_names,auth_token)
+		end		
+
+		return follower_names	
 	end
 
-	def base_following
+	#helper method to parse the extra followers, from a given json response from twitter.
+	def parse_extra_followers(data_cursor_val,profile_name,follower_names,auth_token)
+		#the URI... Magic, really. If this changes, all must change accordingly! We worship this URI! (pls don't change this, Twitter.)
+		uri = "https://twitter.com/#{profile_name}/followers/users?cursor=#{data_cursor_val}&include_available_features=1&include_entities=1&is_forward=true"
+		response = RestClient.get(uri,
+									{:cookies => {:auth_token => auth_token.to_s,:secure_session => "true"}})
 
+		json = JSON.parse(response)
+		file = File.open('auth_page.html','w')
+		file.write(json)
+		file.close
+		#puts JSON.parse(response)
+	#	https://twitter.com/Cmdr_Hadfield/followers/users?cursor=1347834627953995830&include_available_features=1&include_entities=1&is_forward=true
+
+		return false
+	end
+
+	#return a list of the profiles of everyone who this person is following.
+	def base_following(agent,profile_name)
+		following_names = Array.new
+		following_page = agent.click(agent.page.link_with(:href => "/#{profile_name}/following"))
+		following_page.links.each do |link|
+
+		end
+		return following_names
 	end
 
 end
