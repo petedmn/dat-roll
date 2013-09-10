f4b30047ac94b5f3d49a1fe15a04162649c3c69b
Iain Walker <immachumm@gmail.com>
2013-09-10 20:03:14 +1200

    more functionality, can search for pages.

--- twitter/community_detection.rb
+++ twitter/community_detection.rb
@@ -3,20 +3,47 @@ require 'mechanize'
 require 'rest-client'
 require_relative './UserAgent'
 
-
 class Crawler
 
 	#we start with a given profile. 
 	#profile_name = the profile to start with
-	#home page is the mechanize representation of the authenticated home page.
-	def initialize(profile_name,home_page)
+	#agent is the current mechanize object; we are able to use this to remain authenticated through the current page.
+	def initialize(profile_name,agent)
 		@profile_name = profile_name
-		@home_page = home_page 
+		@agent = agent
 	end	 
 
 	#we need to loop through profiles, and gather follower/following details for these profiles
+	#this is kind of the main method for our crawler.
 
 	#then, we can go through tweets and look at who is retweeting these. 
+	def execute(num_profiles=0) #might start using number of profiles as an argument later;not needed at this point.
+		#first, load the base profile.
+		page = search_page(@agent,@profile_name)
+		
+		#now click the first link on the page, in order to access this page.
+
+	end
+
+	#search for a page with the given profile name
+	def search_page(agent,profile_name)
+		current_page = agent.page
+
+		searched_page = current_page.form_with(:action => '/search') do |form|
+			form.field_with(:name=>'q').value = profile_name
+		end.click_button
+
+		profile_page = agent.click(searched_page.link_with(:href => "/#{profile_name}"))
+		return profile_page
+	end
+
+	def base_followers
+
+	end
+
+	def base_following
+
+	end
 
 end
 
@@ -43,21 +70,19 @@ class Authenticate
 
 	def authenticate
 		puts 'authenticating...'
-		#rest_client = RestClient.new
-		#RestClient.get('http://www.google.com')
 		@agent.get('https://twitter.com/') do |page|
 			puts 'page loaded'
 
 			@auth_page = page.form_with(:action => 'https://twitter.com/signup') do |form| #the form that we need!
-				puts form.action
-				form.fields.each do |field|
-					puts field.name					
-				end
 				form.field_with(:name => 'user[email]').value = @username
 				form.field_with(:name => 'user[user_password]').value = @password
 			end.click_button
 		end	
 		
+	end
+
+	def get_agent
+		return @agent
 	end
 
 	def get_home_page
@@ -91,4 +116,6 @@ auth = Authenticate.new('engr489@gmail.com','smokey12#')
 auth.authenticate
 
 #then begin the algorithm..
-crawler = Crawler.new('cmdrhadfield',auth.get_home_page)
+#current implementation is highly dependant on the username being EXACTLY correct. Therefore pages which I've already fetched are prime candidates.
+crawler = Crawler.new('Cmdr_Hadfield',auth.get_agent)
+crawler.execute #add arguments for number of pages to scrape, potentially.
